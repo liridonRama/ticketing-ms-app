9a2bbe0f5ea1fd08c4fc2b134dca083d7c6afa56
liridon.rama <liridon.rama@me.com>
2022-02-01 07:08:30 +0100

    code cleanup

--- tickets-srv/src/routes/create-ticket-router.ts
+++ tickets-srv/src/routes/create-ticket-router.ts
@@ -1,6 +1,6 @@
 import { requireAuth, validateRequest } from '@ticketing-lr/common';
-import { body } from 'express-validator';
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 import { Ticket } from '../../models/tickets';
 
 const router = express.Router();
