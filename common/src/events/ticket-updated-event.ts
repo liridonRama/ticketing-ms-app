129435e46919d33b9b2f59b1f1ed696ad725256c
liridon.rama <liridon.rama@me.com>
2022-02-13 19:57:44 +0100

    Updates

--- common/src/events/ticket-updated-event.ts
+++ common/src/events/ticket-updated-event.ts
@@ -1,6 +1,6 @@
 import { Subjects } from './subjects';
 
-export interface TicketupdatedEvent {
+export interface TicketUpdatedEvent {
   subject: Subjects.TicketUpdated;
   data: {
     id: string;
